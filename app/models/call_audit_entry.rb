84331f1b63771de84ea14184d644b1ee029231c9
achaudhary123 <achaudhary@enbake.com>
2013-11-22 10:04:50 +0530

    * added association for comments in call_audit_entry

--- app/models/call_audit_entry.rb
+++ app/models/call_audit_entry.rb
@@ -3,5 +3,6 @@ class CallAuditEntry < ActiveRecord::Base
   belongs_to :carrier_branch
   belongs_to :reviewer, :class_name => 'Employee'
   has_many :call_audit_detail_files,:as => :reference
+  has_many :claim_audit_comments, :as => :reference
   validates :css_id, presence: true, uniqueness: true
 end
